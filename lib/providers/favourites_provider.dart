bc106f9a71e23b2b3e5b68024502b3645578a0d6
Manasvi143 <manasvijangra31@gmail.com>
2023-09-10 18:11:51 +0530

    Short animation

---
+++ lib/providers/favourites_provider.dart
@@ -0,0 +1,4 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:meals_app/model/meal.dart';
+
+// class FavouriteMealsNotifier extends StateNotifier<List<Meal>>{}
--- lib/screens/catagory_screen.dart
+++ lib/screens/catagory_screen.dart
@@ -7,15 +7,40 @@ import 'package:meals_app/widgets/catagory_grid_item.dart';
 
 import '../data/dummy_catagory.dart';
 
-class CatagoryScreen extends StatelessWidget {
+class CatagoryScreen extends StatefulWidget {
   const CatagoryScreen(
       {super.key, required this.toggleFavourite, required this.availableMeals});
 
   final void Function(Meal meal) toggleFavourite;
   final List<Meal> availableMeals;
 
+  @override
+  State<CatagoryScreen> createState() => _CatagoryScreenState();
+}
+
+class _CatagoryScreenState extends State<CatagoryScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      // lowerBound: 0,
+      // upperBound: 1,
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCatagory(BuildContext context, Category category) {
-    final filterdMeal = availableMeals
+    final filterdMeal = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -23,17 +48,15 @@ class CatagoryScreen extends StatelessWidget {
       builder: (context) => MealScreen(
           title: category.title,
           meals: filterdMeal,
-          toggleFavourite: toggleFavourite),
+          toggleFavourite: widget.toggleFavourite),
     ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Select a catagory'),
-      // ),
-      body: GridView(
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
           padding: const EdgeInsets.all(24),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
@@ -50,6 +73,10 @@ class CatagoryScreen extends StatelessWidget {
                     _selectCatagory(context, temp);
                   }),
           ]),
+      builder: (context, child) => Padding(
+        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+        child: child,
+      ),
     );
   }
 }
